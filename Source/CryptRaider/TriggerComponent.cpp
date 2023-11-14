943f909a7ea2384d4058404870d13cc73eaa2a66
sayaIvy <saya6414h@gmail.com>
2023-11-15 03:51:26 +0900

    [add] Collision added to gimmick wall

---
+++ Source/CryptRaider/TriggerComponent.cpp
@@ -0,0 +1,11 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "TriggerComponent.h"
+
+// Called when the game starts
+void UTriggerComponent::BeginPlay()
+{
+	Super::BeginPlay();	
+  UE_LOG(LogTemp, Display, TEXT("trigger component"));
+}
