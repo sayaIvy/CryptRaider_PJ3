2e4a25eb7d9fedea2d6f1fb2f96b4c893c49cb21
sayaIvy <saya6414h@gmail.com>
2023-11-15 04:29:40 +0900

    [add] Stores Contacted Actor in TArray

--- Source/CryptRaider/TriggerComponent.cpp
+++ Source/CryptRaider/TriggerComponent.cpp
@@ -18,4 +18,13 @@ void UTriggerComponent::BeginPlay()
 void UTriggerComponent::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
 {
   Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+
+  TArray<AActor*> Actors;
+  GetOverlappingActors(Actors);
+
+  if(Actors.Num() > 0)
+  {
+    FString ActorName = Actors[0]->GetActorNameOrLabel();
+    UE_LOG(LogTemp, Display, TEXT("Overlapping: %s"), *ActorName);
+  }
 }
