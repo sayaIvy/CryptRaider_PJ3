ac230b31e6bf95b35cca2948cdb5f0f7442906bb
sayaIvy <saya6414h@gmail.com>
2023-11-15 04:37:05 +0900

    [fix] Remake get TArray list

--- Source/CryptRaider/TriggerComponent.cpp
+++ Source/CryptRaider/TriggerComponent.cpp
@@ -22,9 +22,9 @@ void UTriggerComponent::TickComponent(float DeltaTime, ELevelTick TickType, FAct
   TArray<AActor*> Actors;
   GetOverlappingActors(Actors);
 
-  if(Actors.Num() > 0)
+  for(int32 i = 0; i < Actors.Num(); i++)
   {
-    FString ActorName = Actors[0]->GetActorNameOrLabel();
+    FString ActorName = Actors[i]->GetActorNameOrLabel();
     UE_LOG(LogTemp, Display, TEXT("Overlapping: %s"), *ActorName);
   }
 }
